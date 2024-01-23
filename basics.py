0e880f24972b73fe5ccb2e64f6accd1a750e0fcb
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-22 20:14:38 -0600

    se agrego el archivo basicos de python

---
+++ basics.py
@@ -0,0 +1,29 @@
+
+#imprime en una sola fila
+print("hey steve")
+
+#lo imprime en dos filas
+print("hey\n steve")
+
+#obtener el tipo de un dato en pyton
+type(1)
+type("1")
+type(True)
+type(1.1)
+
+#castear un numero
+int(1.1)
+float(2)
+str(1)
+bool(1)
+
+#operaciones basicas
+print(1+1)
+print(1-1)
+print(1*1)
+print(10/2)
+
+#declarar variables
+nombre="steve"
+edad=21
+altura=1.80
